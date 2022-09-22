472738910fe966e9c723539992bacd91390e0f03
Tanzil <mtanzilislam1997@gmail.com>
2022-09-22 15:09:05 +0100

    fin

--- problems/01-should-recycle.js
+++ problems/01-should-recycle.js
@@ -14,11 +14,12 @@ return `Recycle Me!`
 
 
 function shouldRecycle(item) {
-  if (!item.plastic) {
+  if (item.plastic) {
+     if (item.color === 'black') {
+      return 'Currently, cannot be recycled.'
+    }
     return 'Recycle Me!'
-  } else if (item.color === 'black') {
-    return 'Currently, cannot be recycled.'
-  } else if (item.aluminum) {
+  }  else if (item.aluminum) {
     return 'Recycle Me!'
   } else if (item.paper) {
     return 'Recycle Me!'
